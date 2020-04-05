21ac0961d9c72e0a60bfa59d78cc31609f040998
Carmelo Riolo (Genji) <carmeloriolo93@gmail.com>
2020-04-05 20:03:15 +0200

    Added __author__ and __version__ in main

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
 from modules.inspectors.cpu_inspector import CPUInspector
 
+__author__ = 'Carmelo Riolo (Genji)'
+__version__ = '0.1'
+
 if __name__ == '__main__':
     _cpu_inspector = CPUInspector()
     print(_cpu_inspector.inspect())
